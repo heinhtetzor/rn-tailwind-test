80a5f275eee4488894ec17cfab13be8ca3920eab
heinhtetzor <heinhzor@gmail.com>
2022-09-25 14:56:34 +0800

    update welcome texts

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,26 +1,25 @@
-import React, { useLayoutEffect} from 'react';
+import React, {useLayoutEffect} from 'react';
 import {View, Text, SafeAreaView, Image} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
 
-    useLayoutEffect(() => {
+  useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
   });
-  
+
   return (
     <SafeAreaView>
-      <Text className="text-red-500">
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
-          <View>
-            <Text className="font-bold text-gray-400 text-xs"> some text </Text>
-            <Text className="font-bold text-x;"> Header Title </Text>
-          </View>
-        </View>
-      </Text>
+      <View className="h-screen pb-3 mt-10 backdrop-blur-100 bg-slate-800 backdrop-blur-lg">
+        <Text className="font-bold text-gray-400 text-7xl"> LEEEE LARR </Text>
+        <Text className="font-bold text-8xl text-green-500">
+          {' '}
+          PHINN KHAN LITE{' '}
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
