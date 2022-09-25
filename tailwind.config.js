988c868fc3339a8e2b497eb9564fce2edb439fe2
heinhtetzor <heinhzor@gmail.com>
2022-09-25 14:56:25 +0800

    update tailwind path reg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./<custom-folder>/**/*.{js,jsx,ts,tsx}"],
+  content: ['./App.{js,jsx,ts,tsx}', './screens/**/*.{js,jsx,ts,tsx}'],
   theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+    extend: {},
+  },
+  plugins: [],
+};
